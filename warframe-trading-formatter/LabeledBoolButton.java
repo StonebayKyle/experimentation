ece148c2774205ebbc2f2c65cbf3f16000132a86
StonebayKyle <stonebay.kyle@gmail.com>
2020-03-16 19:17:13 -0700

    Creates classes to handle some label-input pairings

---
+++ warframe-trading-formatter/LabeledBoolButton.java
@@ -0,0 +1,64 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.*;
+
+public class LabeledBoolButton extends JPanel implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
+
+    private JLabel label;
+    private BoolButton boolButton;
+
+    private JLabel defaultLabel;
+
+    private static final int HEIGHT = 32; // should be half of testing height in order to not overlap with whitespace.
+
+    public LabeledBoolButton(int x, int y, String labelText, boolean isOn, String onText, String offText) {
+        // setBounds(x, y, 200, 65);
+        init(labelText, isOn, onText, offText);
+        
+        setBounds(x, y, boolButton.getX()+(int)(boolButton.getWidth()*1.3), HEIGHT);
+    }
+
+    public LabeledBoolButton(int x, int y, String labelText, boolean isOn, String onText, String offText, String defaultLabelText) {
+        // setBounds(x, y, 200, 65);
+        init(labelText, isOn, onText, offText);
+
+        defaultLabel = InitHelper.initLabel(boolButton.getX()+(int)(boolButton.getWidth()*1.1), 0, defaultLabelText);
+        add(defaultLabel);
+
+        setBounds(x, y, defaultLabel.getX()+(int)(defaultLabel.getWidth()*1.3), HEIGHT);
+    }
+
+    private void init(String labelText, boolean isOn, String onText, String offText) {
+        setLayout(null);
+
+        label = InitHelper.initLabel(0, 0, labelText);
+        add(label);
+
+        boolButton = new BoolButton(label.getWidth(), 0, isOn, onText, offText);
+        add(boolButton);
+    }
+
+    public boolean isOn() { return boolButton.isOn(); }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    public static int getHEIGHT() { return HEIGHT; }
+
+    public static void main(String[] args) {
+        LabeledBoolButton labeledBoolButton = new LabeledBoolButton(10, 300, "Linkable Brackets:", true, "ON", "OFF");
+        JFrame frame = new JFrame("LabeledBoolButton Test");
+        frame.add(labeledBoolButton);
+        frame.setSize(labeledBoolButton.getWidth(),labeledBoolButton.getHeight()*2);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+}
---
+++ warframe-trading-formatter/LabeledField.java
@@ -0,0 +1,62 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.event.*;
+
+public class LabeledField extends JPanel implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
+
+    private JLabel label;
+    private JTextField textField;
+
+    private JLabel defaultLabel;
+
+    private static final int HEIGHT = 32; // should be half of testing height in order to not overlap with whitespace.
+
+    public LabeledField(int x, int y, String labelText, String fieldText) {
+        init(labelText, fieldText);
+        setBounds(x, y, textField.getX()+(int)(textField.getWidth()*1.3), HEIGHT);
+    }
+
+    public LabeledField(int x, int y, String labelText, String fieldText, String defaultLabelText) {
+        init(labelText, fieldText);
+
+        defaultLabel = InitHelper.initLabel(textField.getX()+(int)(textField.getWidth()*1.1), 0, defaultLabelText);
+        add(defaultLabel);
+
+        setBounds(x, y, defaultLabel.getX() + (int)(defaultLabel.getWidth()*1.3), HEIGHT);
+    }
+
+    private void init(String labelText, String fieldText) {
+        setLayout(null);
+
+        label = InitHelper.initLabel(0, 0, labelText);
+        add(label);
+        textField = InitHelper.initTextField(label.getWidth(), 0, fieldText);
+        add(textField);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public JTextField getTextField() { return textField; }
+
+    public static int getHEIGHT() { return HEIGHT; }
+
+    public static void main(String[] args) {
+        LabeledField labeledField = new LabeledField(10, 340, "Prefix:", "_item", "(optional)");
+        JFrame frame = new JFrame("LabeledField Test");
+        frame.add(labeledField);
+        frame.setSize(labeledField.getWidth(),labeledField.getHeight()*2);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+    }
+}
