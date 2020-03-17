fed0ba52d56533f2feed659f2815555b1f078937
StonebayKyle <stonebay.kyle@gmail.com>
2020-03-16 19:18:51 -0700

    Creates a combined UI for inputting data and settings

--- warframe-trading-formatter/Screen.java
+++ warframe-trading-formatter/Screen.java
@@ -1,12 +1,27 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-public class Screen extends JPanel {
+import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Screen extends JPanel implements ActionListener {
 
     private static final long serialVersionUID = -5529336786288984928L;
 
     private String title;
     private int width, height;
+
+    private JTextArea centralTextArea;
+
+    private LabeledBoolButton bracketBoolButton;
+    private LabeledBoolButton spacesBetweenBoolButton;
+    private LabeledBoolButton removeOutstandingSpacesBoolButton;
+
+    private LabeledField prefixField;
+    private LabeledField suffixField;
+    private LabeledField separatorField;
 
     private static final int WIDTH_DEFAULT = 800;
     private static final int HEIGHT_DEFAULT = 800;
@@ -39,17 +54,108 @@ public class Screen extends JPanel {
     }
 
     private void init() {
+        setLayout(null);
+        setSize(width, height);
 
-        JFrame frame = new JFrame(title);
-        frame.add(this);
+        JButton formatButton = initFormatButton(100, 40);
+        add(formatButton);
 
-        frame.setSize(width, height);
+        JScrollPane scrollPane = initScrollPane(500, 200);
+        add(scrollPane);
 
-        // frame.pack();
+        initLeftCol();
+
+        CharacterLimitMenu characterLimitMenu = new CharacterLimitMenu(500, 300);
+        add(characterLimitMenu);
+
+    }
+
+    private void initLeftCol() {
+        int offset = 300, numDown = 0; // pixels from top
+        bracketBoolButton = new LabeledBoolButton(10, offset + numDown, "Linkable Brackets:", true, "ON", "OFF");
+        add(bracketBoolButton);
+        numDown += LabeledBoolButton.getHEIGHT();
+
+        prefixField = new LabeledField(10, offset + numDown, "Prefix:", "_item", "(optional)");
+        add(prefixField);
+        numDown += LabeledField.getHEIGHT();
+        suffixField = new LabeledField(10, offset + numDown, "Suffix:", "item_", "(optional)");
+        add(suffixField);
+        numDown += LabeledField.getHEIGHT();
+        
+        separatorField = new LabeledField(10, offset + numDown, "Separation Marker:", "item1*item2", "(optional default: , )");
+        add(separatorField);
+        numDown += LabeledField.getHEIGHT();
+
+        spacesBetweenBoolButton = new LabeledBoolButton(10, offset + numDown, "Put Spaces Between Items: ", false, "ON", "OFF");
+        add(spacesBetweenBoolButton);
+        numDown += LabeledBoolButton.getHEIGHT();
+        removeOutstandingSpacesBoolButton = new LabeledBoolButton(10, offset + numDown, "Remove Outstanding Spaces:", true, "ON", "OFF", "(recommended:ON)");
+        add(removeOutstandingSpacesBoolButton);
+        numDown += LabeledBoolButton.getHEIGHT();
+    }
+
+    private JButton initFormatButton(int w, int h) {
+        JButton formatButton = new JButton("Format!");
+        formatButton.setActionCommand("format");
+        formatButton.addActionListener(this);
+        formatButton.setBounds(width / 2 - (int) (w * .5), 10, w, h); // centered horizontally
+        return formatButton;
+    }
+
+    
+
+    private JScrollPane initScrollPane(int w, int h) {
+        centralTextArea = InitHelper.initTextArea("Paste your items here");
+        JScrollPane scrollPane = new JScrollPane(centralTextArea);
+        scrollPane.setBounds(width / 2 - (int) (w * .5), 80, w, h);
+        
+        return scrollPane;
+    }
+
+
+    private void formatText(String inputFile, String outputFile, String separator) {
+        try {
+            centralTextArea.write(new FileWriter(inputFile, false));
+            ArrayList<String> items = Parser.getItems(inputFile, separator);
+
+            Formatter formatter = new Formatter(items, bracketBoolButton.isOn(), spacesBetweenBoolButton.isOn(), removeOutstandingSpacesBoolButton.isOn(),
+                                        InitHelper.getStringContents(prefixField.getTextField()), InitHelper.getStringContents(suffixField.getTextField()));
+            formatter.setModifications();
+
+            String outputText = formatter.getFinalOutput();
+            centralTextArea.setText(outputText);
+
+            BufferedWriter out = new BufferedWriter(new FileWriter(outputFile));
+            out.write(outputText);
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("format".equals(e.getActionCommand())) {
+            if (centralTextArea.getFont().getStyle() == 0) formatText("input.txt", "output.txt",
+                                                            InitHelper.getStringContents(separatorField.getTextField(), ","));
+        } else {
+            System.out.println("Unknown Action!");
+        }
+    }
+
+    public String getTitle() { return title; }
+
+    public static void main(String[] args) {
+        Screen screen = new Screen();
+        JFrame frame = new JFrame(screen.getTitle());
+        frame.add(screen);
+        frame.setSize(screen.getWidth(), screen.getHeight());
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setVisible(true);
     }
-    
 
 }
