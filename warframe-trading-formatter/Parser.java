8102d6546810c24f331ffd6278757e98ad97f074
StonebayKyle <stonebay.kyle@gmail.com>
2020-03-17 20:23:53 -0700

    Adds code to parse a raw String from a file

--- warframe-trading-formatter/Parser.java
+++ warframe-trading-formatter/Parser.java
@@ -1,36 +1,64 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
 
-    private Parser() {}
+    private Parser() {
+    }
 
-    
     public static ArrayList<String> getItems(String filePath, String separator) {
         File input = new File(filePath);
         Scanner scan = null;
         ArrayList<String> items = new ArrayList<>();
-        
+
         try {
             scan = new Scanner(input);
-            
+
             while (scan.hasNextLine()) {
                 String line = scan.nextLine();
                 String[] lineArray = line.split(separator);
-                
+
                 for (String item : lineArray) {
                     items.add(item);
                 }
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             scan.close();
         }
-        
+
         return items;
+    }
+
+    public static String getRawString(String filepath) throws Exception {
+        String output = "";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+        String st;
+        while ((st = bufferedReader.readLine()) != null) {
+            output += st;
+        }
+
+        bufferedReader.close();
+
+        return output;
+    }
+
+    public static String getRawString(File file) throws Exception {
+        String output = "";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = bufferedReader.readLine()) != null) {
+            output += st;
+        }
+
+        bufferedReader.close();
+
+        return output;
     }
     
 }
