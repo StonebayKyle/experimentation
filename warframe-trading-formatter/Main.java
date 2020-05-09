8918dfa6e261061178a70032f5c789207f11497b
Kyle Steinsvaag <stonebay.kyle@gmail.com>
2020-05-08 19:07:35 -0600

    Moves main testing code to Main.java

--- warframe-trading-formatter/Main.java
+++ warframe-trading-formatter/Main.java
@@ -1,10 +1,15 @@
+import javax.swing.JFrame;
+
 public class Main {
     
     public static void main(String[] args) {
         Screen screen = new Screen();
-        
-        Formatter formatter = new Formatter(Parser.getItems("input.txt", ","));
-        formatter.setModifications();
-        System.out.println(formatter.getFinalOutput());
+        JFrame frame = new JFrame(screen.getTitle());
+        frame.add(screen);
+        frame.setSize(screen.getWidth(), screen.getHeight());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
